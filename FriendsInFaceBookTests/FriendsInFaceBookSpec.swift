11643dae7b53f529424af5bc461aac28a2a77844
Victoria-Kravets <victoriakravets93@gmail.com>
2018-01-15 19:59:49 +0200

    - added FriendsInFaceBookSpec

---
+++ FriendsInFaceBookTests/FriendsInFaceBookSpec.swift
@@ -0,0 +1,25 @@
+//
+//  FriendsInFaceBookSpec.swift
+//  FriendsInFaceBookTests
+//
+//  Created by Viktoria on 1/15/18.
+//  Copyright © 2018 Victoria Kravets. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import FriendsInFaceBook
+
+class FriendsInFaceBookSpec: QuickSpec {
+    override func spec() {
+        describe("FriendsInFaceBookSpec") {
+            context("when using Nimble and Quick") {
+                it("should pass") {
+                    expect(true).to(beTrue())
+                }
+            }
+        }
+    }
+}
+
